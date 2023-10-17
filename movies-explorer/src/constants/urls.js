d294db590d68264cf392bbdd6a956c8b85390f44
Irina Babaeva <irina.a.babaeva@yandex.ru>
2023-10-17 23:57:57 +0300

    README updated

--- movies-explorer/src/constants/urls.js
+++ movies-explorer/src/constants/urls.js
@@ -1,6 +1,6 @@
 const MOVIE_URL = "https://api.nomoreparties.co/beatfilm-movies";
-// const baseUrl = "https://api.movies-explorer.isakh.nomoreparties.co";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.movies-explorer.isakh.nomoreparties.co";
+// const BASE_URL = "http://localhost:3000";
 const MOVIE_IMAGE_URL = "https://api.nomoreparties.co";
 
 export { MOVIE_URL, BASE_URL, MOVIE_IMAGE_URL };
